829017d2c0ed21d78b2a26c73167c254ff415151
Ricardo Martinho <rmartin.estg@gmail.com>
2017-02-28 12:57:13 +0000

    corrected imports

--- src/test/java/TeamMainPageTest.java
+++ src/test/java/TeamMainPageTest.java
@@ -12,7 +12,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 public class TeamMainPageTest {
     private static final String LINKTEXT_RM = "shortbio";
